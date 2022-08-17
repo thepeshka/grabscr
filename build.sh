3555925634a01dcdf9dd534157e0fefd5b3c85e1
Ilya Peshekhonov <ilya@thepeshka.ru>
2022-08-17 03:36:49 +0300

    add appimage build

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
-VERSION=0.0.7
-
-pyinstaller --add-data 'icon.png:.' --add-data 'grabscr.desktop:.' --add-data 'uninstall.sh:.' -n GrabSCR -y grabscr.py
-cd dist || exit
-zip -r -o ./GrabSCR-linux-$VERSION.zip ./GrabSCR
+docker-compose up --build
+VERSION=$(python -c "import src; print(src.__version__)")
+mv ./dist/GrabSCR-x86_64.AppImage ./dist/GrabSCR-$VERSION-x86_64.AppImage
